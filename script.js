33f477056e152a081a4a94add84fc534b0c0658e
mistrydev6 <36978097+mistrydev6@users.noreply.github.com>
2020-08-29 22:43:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+// var a = prompt("Number 1 :");
+// var b = prompt("Number 2 :");
+// var c = Number(a)+Number(b);
+//
+// if (c === 30) {
+// 	alert("NICE");
+// } else {
+// 	alert("DUMB !!!");
+// }
+//
+// console.log("HEY");
+//
+// *************************************
+// 			FUNCTION DECLARATION
+// *************************************
+//
+// function sayHello() {
+// 	console.log("HELLO");
+// 	console.log("HELLO");
+// 	console.log("HELLO");
+// 	console.log("HELLO");
+// 	console.log("HELLO");
+// 	console.log("HELLO");
+// }
+//
+// *************************************
+// 			FUNCTION EXPRESSION	
+// *************************************
+//
+// var sayBye = function(){
+// 	console.log("BYE");
+// 	console.log("BYE");
+// 	console.log("BYE");
+// 	console.log("BYE");
+// 	console.log("BYE");
+// }
+//
+// *************************************
+// 			1 ARGUMENT IN FUNCTIONS	
+// *************************************
+//
+// function sing(song){
+// 	console.log(song);
+// }
+//
+// sing("AAA");
+// sing("BBB");
+//
+// *************************************
+// 		RETURN-ARGUMENTS IN FUNCTIONS	
+// *************************************
+//
+// function multiply(a, b){
+// 	return a * b;
+// }
+//
+// *************************************
+// 				 ARRAYS	
+// *************************************
+//
+// var a = ["A","B","C"];
+//
+// *************************************
+// 				 OBJECTS	
+// *************************************
+//
+// var a = {
+// 	name: "Dev",
+// 	age: 19,
+// 	place: "India",
+// };
+//
+// *************************************
+// 			 OBJECTS IN ARRAY	
+// *************************************
+//
+// var a = [
+// 	{
+// 		name: "A",
+// 		age: 19,
+// 		place: "India",
+// 	},
+// 	{
+// 		name: "B",
+// 		age: 9,
+// 		place: "US",
+// 	},
+// 	{
+// 		name: "C",
+// 		age: 39,
+// 		place: "UK",
+// 	}
+// ]
+//
+// *************************************
+// 				 LOOPS	
+// *************************************
+//
+var todo = [
+	"clean room",
+	"shopping",
+	"brush",
+	"ok",
+	"done"
+];
+
+//************** FOR LOOP **************
+// for(var i = 0; i < todo.length; i++){ 
+// 	todo[i] = todo[i] + " !";
+// } 
+
+//************** ForEach LOOP **********
+// todo.forEach(function(i){
+// 	console.log(i);
+// })
+
+//************** WHILE LOOP ************
+// var a = 10;
+// while (a > 0) {
+// 	console.log(a);
+// 	a--
+// }
+
+//************** DO-WHILE LOOP **********
+// var b = 10;
+// do {
+// 	console.log(b); 
+// 	b--;
+// } while (b > 0);
+
+
+
+
